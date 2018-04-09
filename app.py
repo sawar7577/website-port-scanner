0eec3aa5ef9bfaa4b21f274ae643b7799885dd1a
b00merang <sawar.sagwal@gmail.com>
2018-04-10 01:17:06 +0530

    added pages for tcp and udp scan

--- app.py
+++ app.py
@@ -13,5 +13,26 @@ def home():
 	else:
 		return render_template("home.html",items=items)
 
+@app.route('/tcp',methods=['POST','GET'])
+def tcp():
+	items=[]
+	if request.method == 'POST':
+		website=request.form['website']
+		items=scn.do_scan(website,"sV")
+		return render_template("scans.html",items=items)
+	else:
+		return render_template("scans.html",items=items)
+
+@app.route('/udp',methods=['POST','GET'])
+def udp():
+	items=[]
+	if request.method == 'POST':
+		website=request.form['website']
+		items=scn.do_scan(website,"sU")
+		return render_template("scans.html",items=items)
+	else:
+		return render_template("scans.html",items=items)
+
+
 if __name__ == "__main__":
 	app.run(debug=True)
